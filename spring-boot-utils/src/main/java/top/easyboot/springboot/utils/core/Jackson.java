6b961e178e60e73b1bb18466c0188c5d5fc4f5bb
余重桦 <yuchonghua@163.com>
2019-07-29 16:32:03 +0800

    Update Jackson.java

--- spring-boot-utils/src/main/java/top/easyboot/springboot/utils/core/Jackson.java
+++ spring-boot-utils/src/main/java/top/easyboot/springboot/utils/core/Jackson.java
@@ -1,7 +1,9 @@
 package top.easyboot.springboot.utils.core;
 
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -9,28 +11,38 @@ import java.util.Map;
 
 public class Jackson {
     private static ObjectAndUrlencodMapper objectMapper = null;
-    public final static ObjectAndUrlencodMapper getObjectMapper(){
+    public static ObjectAndUrlencodMapper getObjectMapper(){
         if (objectMapper==null){
             objectMapper = new ObjectAndUrlencodMapper();
         }
         return objectMapper;
     }
-    public final static String toJson(Object object) throws JsonProcessingException {
+    public static String toJson(Object object) throws JsonProcessingException {
         return getObjectMapper().writeValueAsString(object);
     }
-    public final static void writeAsUrlencod(Object data, OutputStream out) throws IOException {
+    public static void writeAsUrlencod(Object data, OutputStream out) throws IOException {
         getObjectMapper().writeAsUrlencod(data, out);
     }
-    public final static byte[] writeAsUrlencod(Object data) throws JsonProcessingException{
+    public static byte[] writeAsUrlencod(Object data) throws JsonProcessingException{
         return getObjectMapper().writeAsUrlencod(data);
     }
-    public final static String writeAsUrlencodString(Object data) throws JsonProcessingException{
+    public static String writeAsUrlencodString(Object data) throws JsonProcessingException{
         return getObjectMapper().writeAsUrlencodString(data);
     }
-    public final static Map readUrlencod(String content) throws IOException{
+    public static Map readUrlencod(String content) throws IOException{
         return getObjectMapper().readUrlencod(content);
     }
-    public final static <T> T readUrlencod(String content, Class<T> valueType) throws IOException {
+    public static <T> T readUrlencod(String content, Class<T> valueType) throws IOException {
         return getObjectMapper().readUrlencod(content, valueType);
+    }
+    public static Map readValue(String content)
+            throws IOException, JsonParseException, JsonMappingException
+    {
+        return getObjectMapper().readValue(content, Map.class);
+    }
+    public static <T> T readValue(String content, Class<T> valueType)
+            throws IOException, JsonParseException, JsonMappingException
+    {
+        return getObjectMapper().readValue(content, valueType);
     }
 }
